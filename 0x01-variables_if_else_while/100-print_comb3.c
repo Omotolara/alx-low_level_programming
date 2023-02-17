577b121b1bdb9efab87f02a4308f8a5eba4f26c0
Ibukunoluwa Omotola <ibkomotola4real@gmail.com>
2023-02-17 12:37:50 -0800

    Print combination of two numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,19 +8,21 @@
  */
 int main(void)
 {
-	int d;
+	 int i, j;
 
-	for (d = 0; d < 100; d++)
-	{
-		putchar((d / 10) + '0');
-		putchar((d % 10) + '0');
-		if (d != 89)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
-	putchar('\n');
+    for (i = 0; i < 10; i++) {
+        for (j = i + 1; j < 10; j++) {
+            putchar(i + '0');
+            putchar(j + '0');
 
-	return (0);
+            if (i == 8 && j == 9) {
+                putchar('\n');
+            } else {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+
+    return 0;
 }
