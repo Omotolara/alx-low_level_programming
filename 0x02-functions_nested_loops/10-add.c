2671f82c70f1f323185a396ec4172b8c340b9ba6
Ibukunoluwa Omotola <ibkomotola4real@gmail.com>
2023-02-21 14:06:26 -0800

    100 times table

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * add - adds two integers and returns the result
- * @one - first number
- * @two - second number
+ * @one: first number input
+ * @two:  second number input
  * Return: one + two
  */
 int add(int one, int two)
