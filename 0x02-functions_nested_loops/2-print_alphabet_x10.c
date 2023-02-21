b231a86057e21ece7915af859ad54f7e86b79e3e
Ibukunoluwa Omotola <ibkomotola4real@gmail.com>
2023-02-21 13:06:40 -0800

    print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet in lowercase, followed by a new line
+ *
+ * Return: x10 a-z
+ */
+void print_alphabet_x10(void)
+{
+	int letter, times;
+
+	times = 0;
+
+	while (times < 10)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		times++;
+		_putchar('\n');
+	}
+}
