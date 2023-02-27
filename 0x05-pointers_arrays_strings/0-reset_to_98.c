4161746348f96e48841eaa5f3c72e4dfa145b1c4
Ibukunoluwa Omotola <ibkomotola4real@gmail.com>
2023-02-27 07:55:44 -0800

    Reset value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - points to an integer and updates the value of the integer to 98
+  * @n: integer pointed to
+  *
+  * Return: nothing
+  */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
