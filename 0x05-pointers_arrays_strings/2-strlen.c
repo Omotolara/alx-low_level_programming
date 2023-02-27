256ca83c4fbd8ac87886af8249cdeca3dfbf7da6
Ibukunoluwa Omotola <ibkomotola4real@gmail.com>
2023-02-27 09:48:05 -0800

    length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * _strlen - returns the length of a string
+  * @s: the string
+  *
+  * Return: length of string
+  */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	for (; *s != '\0'; s++)
+	{
+		c++;
+	}
+
+	return (c);
+}
