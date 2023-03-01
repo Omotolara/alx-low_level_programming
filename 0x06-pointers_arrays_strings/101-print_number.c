604b31b77e48674bf27fd4cfc2f6b500cc445ccc
Ibukunoluwa Omotola <ibkomotola4real@gmail.com>
2023-03-01 11:45:51 -0800

    print integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * print_number - prints an integer using _putchar
+  * @n: the integer
+  *
+  * Return: nothing
+  */
+void print_number(int n)
+{
+	unsigned int y;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+
+	y = n;
+
+	if (y / 10)
+		print_number(y / 10);
+
+	_putchar(y % 10 + '0');
+}
