4162a1cf492262922a48ad5403e6f4765d34434a
Ibukunoluwa Omotola <ibkomotola4real@gmail.com>
2023-03-02 12:54:09 -0800

    infinite add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
-/* 
+/**
  * infinite_add - adds two numbers
  * @n1: 1st number
  * @n2: 2nd number
  * @r: buffer to store result
  * @size_r: buffer size
+ *
  * Return: pointer to the result
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
@@ -21,7 +22,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	b--;
 	if (b >= size_r || a >= size_r)
 		return (0);
-	while (b >=0 || a >=0 || buff == 1)
+	while (b >= 0 || a >= 0 || buff == 1)
 	{
 		if (a < 0)
 			num1 = 0;
@@ -45,7 +46,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	}
 	if (dg == size_r)
 		return (0);
-	*(r +dg) = '0';
+	*(r + dg) = '0';
 	rev_string(r);
 
 	return (r);
