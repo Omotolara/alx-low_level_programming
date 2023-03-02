912477799d0410b77ecd168d7a446c6ee0c8ec4b
Ibukunoluwa Omotola <ibkomotola4real@gmail.com>
2023-03-02 12:41:47 -0800

    infinite add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -10,51 +10,43 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1 = strlen(n1);
-	int len2 = strlen(n2);
+	int buff = 0, a = 0, b = 0, dg = 0;
+	int num1 = 0, num2 = 0, total = 0;
 
-	if (len1 + len2 + 1 > size_r)
+	while (*(n1 + a) != '\0')
+		a++;
+	while (*(n2 + b) != '\0')
+		b++;
+	a--;
+	b--;
+	if (b >= size_r || a >= size_r)
+		return (0);
+	while (b >=0 || a >=0 || buff == 1)
 	{
-		return 0;
+		if (a < 0)
+			num1 = 0;
+		else
+			num1 = *(n1 + a) - '0';
+		if (b < 0)
+			num2 = 0;
+		else
+			num2 = *(n2 + b) - '0';
+		total = num1 + num2 + buff;
+		if (total >= 10)
+			buff = 1;
+		else
+			buff = 0;
+		if (dg >= (size_r - 1))
+			return (0);
+		*(r + dg) = (total % 10) + '0';
+		dg++;
+		b--;
+		a--;
 	}
+	if (dg == size_r)
+		return (0);
+	*(r +dg) = '0';
+	rev_string(r);
 
-	int carry = 0;
-	int i = len1 - 1;
-	int j = len2 - 1;
-	int k = 0;
-
-	while (i >= 0 || j >= 0)
-	{
-		int digit1 = (i >= 0) ? n1[i] - '0' : 0;
-		int digit2 = (j >= 0) ? n2[j] - '0' : 0;
-		int sum = digit1 + digit2 + carry;
-
-		r[k] = (sum % 10) + '0';
-		carry = sum / 10;
-
-		i--;
-		j++;
-		k++;
-	}
-
-	if (carry != 0)
-	{
-		if (k >= size_r - 1)
-		{
-			return 0;
-		}
-		r[k] = carry + '0';
-		k++;
-	}
-
-	r[k] = '\0';
-
-	for (int i = 0, j = k - 1; i < j; i++, j--)
-	{
-		char temp = r[i];
-		r[i] = r[j];
-		r[j] = temp;
-	}
-
-	return r;
+	return (r);
 }
---
+++ 0x06-pointers_arrays_strings/rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * rev_string - reverses a string
+  * @s: the string
+  *
+  * Return: nothing
+  */
+void rev_string(char *s)
+{
+	char ch, *p, *q;
+	
+	for (q = s; *q != '\0'; ++q)
+	{
+		if (q > s)
+			--q;
+	}
+	for (p = s; p < q; ++p, --q)
+	{
+		ch = *p;
+		*p = *q;
+		*q = ch;
+	}
+}
