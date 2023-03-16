61c916d29481353255198759491c017bbfdb9518
Ibukunoluwa Omotola <ibkomotola4real@gmail.com>
2023-03-16 10:41:43 -0700

    array of int

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,20 +9,23 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *ar;
-	unsigned int ar_size, i;
+	int i = 0, l = 0;
+	char *p;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	ar_size = nmemb * size;
-	ar = malloc(ar_size);
-	if (ar == NULL)
+
+	l = nmemb * size;
+	p = malloc(l);
+
+	if (p == NULL)
 		return (NULL);
-	while (i < ar_size)
+
+	while (i < l)
 	{
-		ar[i] = 0;
+		p[i] = 0;
 		i++;
 	}
 
-	return (ar);
+	return (p);
 }
