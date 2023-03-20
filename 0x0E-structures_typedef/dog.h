a75b8ccffb838cc81ef82aa6c49001e2de2ba566
Ibukunoluwa Omotola <ibkomotola4real@gmail.com>
2023-03-20 07:17:27 -0700

    struct for dog definition

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_STRUCT
+#define DOG_STRUCT
+
+/**
+  * struct dog - structure for a Dog definition
+  * @name: the name of the dog.
+  * @age: the age of the dog.
+  * @owner: the owner of the dog.
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
