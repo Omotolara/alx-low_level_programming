ef08eb49f1e44f3ccfa16c154313a8d4e074a207
Ibukunoluwa Omotola <ibkomotola4real@gmail.com>
2023-03-30 15:34:54 -0700

    prints string before main function is executed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+
 /**
-  * b - prints from init by the loader before main
-  */
-void bmain(void) __attribute__ ((constructor));
-void bmain(void)
+ * b - prints from init by the loader before main
+ */
+void first(void) __attribute__ ((constructor));
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
