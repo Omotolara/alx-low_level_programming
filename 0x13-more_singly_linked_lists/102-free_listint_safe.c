c07585dd958374d7ed045f187761704d9a80dde5
Ibukunoluwa Omotola <ibkomotola4real@gmail.com>
2023-04-03 14:26:15 -0700

    safely frees a list

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include<stdio.h>
 
+/**
+ * free_listint_safe - frees a list and set the head to null
+ * @h: double pointer to head of linked list
+ * Description: This function should work for circular lists
+ * Only loop through the list once
+ * Return: size of the list that was free'd
+ */
 size_t free_listint_safe(listint_t **h)
 {
 	listint_t *current, *hold;
