734469612fe31a1df0a235321c1e73f0263cb4b4
Ibukunoluwa Omotola <ibkomotola4real@gmail.com>
2023-04-03 11:50:08 -0700

    prints elements of a linked list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 /**
-  * print_listint - prints all the elements of a linked list
-  * @h: head of the linked list
-  *
-  * Return: number of nodes of the linked list
-  */
+ * print_listint - prints all the elements of a linked list
+ * @h: head of the linked list
+ *
+ * Return: number of nodes of the linked list
+ */
 size_t print_listint(const listint_t *h)
 {
 	int count = 0;
