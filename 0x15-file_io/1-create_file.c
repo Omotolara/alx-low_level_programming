ca6aee774364dba81b6422098cdb9dd8ca476483
Ibukunoluwa Omotola <ibkomotola4real@gmail.com>
2023-04-10 15:37:33 -0700

    creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		while (text_content[str_len])
-			len++;
+			str_len++;
 		bwr = write(file_desc, text_content, str_len);
 		if (bwr != str_len)
 		{
