1933d0c9df75a4891af673977aeae9b66361dc61
Ibukunoluwa Omotola <ibkomotola4real@gmail.com>
2023-04-10 08:40:21 -0700

    binary rep of a number

--- 0x14-bit_manipulation/_putchar.c
+++ 0x14-bit_manipulation/_putchar.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _write - prints a character
+ * _putchar - prints a character
  * @c: the character
  * Return: 1 if successful, -1 if error occurs.
  */
-int _write(char c)
+int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
