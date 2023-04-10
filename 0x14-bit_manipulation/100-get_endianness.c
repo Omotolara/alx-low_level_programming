8fd494177de93eba7362cb399de56c8bc9a0901c
Ibukunoluwa Omotola <ibkomotola4real@gmail.com>
2023-04-10 10:41:48 -0700

    checks endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned int num = 1;
+	char *p = (char *)&num;
+
+	if (int *p == 0)
+	{
+		return (0);
+	}
+	else if (int *p == 1)
+	{
+		return (1);
+	}
+}
