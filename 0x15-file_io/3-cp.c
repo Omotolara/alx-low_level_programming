a1193a3cf39eda0d1e78a14af377691c50dc2372
Ibukunoluwa Omotola <ibkomotola4real@gmail.com>
2023-04-29 16:17:36 -0700

    copy content of file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,38 +14,6 @@ void _stderr(int cd, char *msg, int file_desc)
 }
 
 /**
- * cp_file - copies file
- * @path_src: the path copying from
- * @path_dest: error message
- * Return: nothing
- */
-void cp_file(char *path_src, char *path_dest)
-{
-	int file_src, file_dest, brd, bwr;
-	char buff[BUFF_SIZE];
-
-	file_src = open(path_src, 0_RDONLY);
-	if(file_src  == -1)
-		_stderr(98, "Error: Can't read from file %s\n", path_src);
-	file_dest = open(path_dest, O_CREAT | O_TRUNC | O_WRONLY, 0664);
-	if (file_dest == -1)
-		_stderr(99, "Error: Can't write to %s\n", path_dest);
-	brd = read(file_src, buff, BUFF_SIZE);
-	while (brd > 0)
-	{
-		bwr = write(file_src, buff, brd);
-		if (bwr == -1)
-			_stderr(99, "Error: Can't write to %s\n", path_src);
-        }
-	if (rd == -1)
-		_stderr(98, "Error: Can't read from file %s\n", path_dest);
-	if (close(file_dest) == -1)
-		_stderr(100, "Error: Can't close fd %d\n", file_dest);
-	if (close(file_src) == -1)
-		_stderr(100, "Error: Can't close fd %d\n", file_src);
-}
-
-/**
  * main - copies the content of a file to another file
  * @argc: the argument count
  * @argv: the argument vector
@@ -53,9 +21,30 @@ void cp_file(char *path_src, char *path_dest)
  */
 int main(int argc, char *argv[])
 {
+	int file_src, file_dest, brd, bwr;
+	char buff[BUFF_SIZE];
+
 	if (argc != 3)
 		_stderr(97, "Usage: cp file_from file_to\n", 0);
-	cp_file(argv[1], argv[2]);
+	file_src = open(argv[1], O_RDONLY);
+	if (file_src == -1)
+		_stderr(98, "Error: Can't read from file %s\n", argv[1]);
+	file_dest = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	if (file_dest == -1)
+		_stderr(99, "Error: Can't write to %s\n", argv[2]);
+	brd = read(file_src, buff, BUFF_SIZE);
+	while (brd > 0)
+	{
+		bwr = write(file_dest, buff, brd);
+		if (bwr == -1)
+			_stderr(99, "Error: Can't write to %s\n", argv[2]);
+	}
+	if (brd == -1)
+		_stderr(98, "Error: Can't read from file %s\n", argv[1]);
+	if (close(file_src) == -1)
+		_stderr(100, "Error: Can't close fd %d\n", file_src);
+	if (close(file_dest) == -1)
+		_stderr(100, "Error: Can't close fd %d\n", file_dest);
 
 	return (0);
 }
