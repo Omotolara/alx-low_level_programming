a2d104f779e56bdc4483859c3be91c920e5f5e41
omotolara <ibkomotola4real@gmail.com>
2023-08-25 05:20:23 -0700

    C functions from Python

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,51 @@
+#include "stdio.h"
+
+/**
+ * add: performs addition operation
+ * @a: first number
+ * @b: second number
+ * Return: the addition
+ */
+int add(int a, int b) {
+	return (a + b);
+}
+
+/**
+ * sub: performs subtraction operation
+ * @a: first number
+ * @b: second number
+ * Return: the result
+ */
+int sub(int a, int b) {
+	return (a - b);
+}
+
+/**
+ * mul: performs multiplication operation
+ * @a: first number
+ * @b: second number
+ * Return: the result
+ */
+int mul(int a, int b) {
+	return (a * b);
+}
+
+/**
+ * div: performs division operation
+ * @a: first number
+ * @b: second number
+ * Return: the result
+ */
+int div(int a, int b) {
+	return (a / b);
+}
+
+/**
+ * mod: performs modulo operation
+ * @a: first number
+ * @b: second number
+ * Return: the result
+ */
+int mod(int a, int b) {
+	return (a % b);
+}
